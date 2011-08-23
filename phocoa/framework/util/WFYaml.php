d80f0894cbdd17ce14538a9eba68295399f74203
Jason Ardell <ardell@gmail.com>
2011-08-23 14:57:43 -0400

    Bug fix: stop throwing an erroneous exception when parsing empty yaml files and strings with libyaml, instead simply return array().

--- phocoa/framework/util/WFYaml.php
+++ phocoa/framework/util/WFYaml.php
@@ -2,15 +2,36 @@
 
 class WFYaml
 {
+
+    /**
+     * Make sure that the parsed yaml result was not an error.
+     * Empty array() IS a valid result, but NULL is an invalid
+     * result and should throw an exception.
+     *
+     * @throws Exception If the result returned from the yaml parser is NULL (i.e. the YAML parser couldn't parse the file/string)
+     */
+    private static function assertValidYamlParseResult($result, $errorMessage)
+    {
+        // We need to check $result === NULL here because simply checking
+        // the truthiness of $result would evaluate empty array() as false
+        // meaning that empty yaml files (or strings) would result in parse
+        // exceptions instead of empty (valid) parse results.
+        if ($result === NULL) throw new Exception($errorMessage);
+    }
+
+    /**
+     * Load and parse a yaml file, returning a php hash.
+     *
+     * @param The file path to parse as yaml.
+     * @return array The parsed yaml file represented as a php hash.
+     * @throws Exception If the yaml parser was unable to parse the file as valid yaml.
+     */
     public static function loadFile($file)
     {
         if (function_exists('yaml_parse_file'))
         {
             $a = yaml_parse_file($file);
-            if (!$a)
-            {
-                throw new WFException("Error processing YAML file: {$file}");
-            }
+            self::assertValidYamlParseResult($a, "Error processing YAML file: {$file}");
             return $a;
         }
         else if (function_exists('syck_load'))
@@ -39,18 +60,23 @@ class WFYaml
     }
 
     /**
+     * Parse yaml from a string. If yaml is invalid an Exception will
+     * be thrown, otherwise a php hash will be returned.
+     *
      * NOTE: libsyck extension doesn't have a 'string' loader, so we have to write a tmp file. Kinda slow... in any case though shouldn't really use YAML strings
      * for anything but testing stuff anyway
      *
-     * @param 
-     * @return
-     * @throws
+     * @param The string to parse.
+     * @return array The parsed string represented as a php hash.
+     * @throws Exception If the yaml parser was unable to parse the string as valid yaml.
      */
     public static function loadString($string)
     {
         if (function_exists('yaml_parse'))
         {
-            return yaml_parse($string);
+            $result = yaml_parse($string);
+            self::assertValidYamlParseResult($result, "Error processing YAML string");
+            return $result;
         }
         else if (function_exists('syck_load'))
         {
